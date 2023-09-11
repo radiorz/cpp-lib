369a0e9f10f8ae67697f89c685fe5912d07fed2d
tikkhun <s565338107@163.com>
2023-09-11 22:53:30 +0800

    chore: 试了一下python不行啊

--- python-use/index.py
+++ python-use/index.py
@@ -1,1 +1,10 @@
-print('你好')
+import ctypes
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+
+lib = ctypes.CDLL(os.path.join(current_dir, '..', 'libhello', 'build', 'libhello.dll'))
+
+result = lib.add(1,2)
+lib.hello("world")
