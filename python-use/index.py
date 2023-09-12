4a432a8190e946a34457f04641f00305127ff2e4
tikkhun <s565338107@163.com>
2023-09-12 10:25:52 +0800

    chore(python-use_: 还没成功

--- python-use/index.py
+++ python-use/index.py
@@ -1,10 +1,14 @@
-import ctypes
+from ctypes import *
 import os
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
-
-lib = ctypes.CDLL(os.path.join(current_dir, '..', 'libhello', 'build', 'libhello.dll'))
-
+# the_path= os.path.join(current_dir,'libhello.dll')
+the_path= 'libhello.dll'
+cdll.LoadLibrary(the_path)  
+lib = CDLL(the_path)       
+# lib.add.argtypes = [ctypes.c_int, ctypes.c_int]
+# lib.add.restype = ctypes.c_int
 result = lib.add(1,2)
-lib.hello("world")
+# lib.hello("world")
+ 
