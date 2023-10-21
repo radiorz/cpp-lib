c6eb5011fbb306884c1e9657892bb2eb5acd7742
tikkhun <s565338107@163.com>
2023-10-21 08:58:06 +0800

    chore: 导出书写规范

--- libhello/lib/hello.h
+++ libhello/lib/hello.h
@@ -4,9 +4,17 @@
 #include <string>
 using namespace std;
 
+// 变成c 才能被调用
+#ifdef __cplusplus
 extern "C"
-{ 
+{
+#endif
+
   void hello(string);
-  int add(int,int);
+  int add(int, int);
+
+#ifdef __cplusplus
 }
+#endif
+
 #endif
